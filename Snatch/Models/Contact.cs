d5197654fa66b8cd9d37decd5e7b7de65b304980
Ron <ronronya@gmail.com>
2017-03-03 11:13:04 -0800

    Ron - Team page Fix

---
+++ Snatch/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Snatch.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+
+    }
+}
